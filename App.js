14a5a55a721df05ab328bb7a3b974c940bbd02c7
ghidyon <gideonakunana@gmail.com>
2021-12-11 16:15:06 +0100

    practiced dimensions

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, SafeAreaView, Alert, Button } from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  StatusBar,
+  Platform
+} from 'react-native';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
-  const handlePress = () => console.log("Pressed!!!");
-  console.log("App executed!");
-
+  const { landscape, potrait } = useDeviceOrientation();
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <Button
-        color="orange"
-        onPress={() => Alert.prompt("Number Prompt", "Enter a number",
-          (string => console.log(string)))}
-        title="Tap me" />
-    </SafeAreaView>
+      <View style={{
+        width: '100%',
+        height: landscape ? '100%' : '30%',
+        backgroundColor: "dodgerblue"
+      }}>
+
+      </View>
+    </SafeAreaView >
   );
 }
 
@@ -22,7 +28,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
